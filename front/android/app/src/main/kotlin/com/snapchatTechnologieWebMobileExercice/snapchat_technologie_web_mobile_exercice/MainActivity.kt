9f9b71eda6de5f1e077c457145415c68f3b1598f
Ademaus <amaury.delassus@gmail.com>
2022-09-23 15:50:28 +0200

    Add files

---
+++ front/android/app/src/main/kotlin/com/snapchatTechnologieWebMobileExercice/snapchat_technologie_web_mobile_exercice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.snapchatTechnologieWebMobileExercice.snapchat_technologie_web_mobile_exercice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ front/build/app/generated/source/buildConfig/debug/com/snapchatTechnologieWebMobileExercice/snapchat_technologie_web_mobile_exercice/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.snapchatTechnologieWebMobileExercice.snapchat_technologie_web_mobile_exercice;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.snapchatTechnologieWebMobileExercice.snapchat_technologie_web_mobile_exercice";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
