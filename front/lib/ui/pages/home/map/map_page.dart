4fdaa433b47d659ec17049dae33888def50bb8b8
Nintendoboy59 <nintendoboy507@gmail.com>
2022-10-21 20:07:13 +0200

    ✅ Map Page (#6)
    * add picture to map page
    
    * add button in map page

--- front/lib/ui/pages/home/map/map_page.dart
+++ front/lib/ui/pages/home/map/map_page.dart
@@ -10,8 +10,50 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('map page'),
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: FittedBox(
+            child: Image.asset('assets/images/mapPicture.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(
+                  left: 0.0,
+                  right: 5.0,
+                  top: 10.0,
+                  bottom: 5.0,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                  },
+                  child: Container(
+                    height: 55,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: const Center(
+                      child: Text(
+                        'Trouve tes amis',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                )),
+          ],
+        ),
+      ],
     );
   }
 }
